50dd29b1a9bc0118bfd2eac0168a0c838db72e57
J.D. Zamfirescu-Pereira <jd@zamfi.net>
2014-04-26 17:45:02 -0700

    small fixes

--- simple-activated-light.ino
+++ simple-activated-light.ino
@@ -23,14 +23,14 @@ void setup() {
 }
 
 void loop() {
-  if (analogRead(sensorPin) < 200) {
+  if (analogRead(sensorPin) < 500) {
     // go to yellow and red on the thoroughfare.
     digitalWrite(greenPin, LOW);
     digitalWrite(yellowPin, HIGH);
-    delay(3600);
+    delay(3200);
     digitalWrite(yellowPin, LOW);
     digitalWrite(redPin, HIGH);
-    delay(2000);
+    delay(1000);
 
     // go to green on the side street.
     digitalWrite(redPin2, LOW);
@@ -40,10 +40,10 @@ void loop() {
     // go to yellow and red on the side street.
     digitalWrite(greenPin2, LOW);
     digitalWrite(yellowPin2, HIGH);
-    delay(3600);
+    delay(3200);
     digitalWrite(yellowPin2, LOW);
     digitalWrite(redPin2, HIGH);
-    delay(2000);
+    delay(1000);
     
     // go back to green on the thoroughfare
     digitalWrite(redPin, LOW);
